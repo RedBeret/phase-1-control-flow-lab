013badbd8c12cfc62e8c0ab246ac6a596f7b8c90
Steven Espinoza <espisteven5@gmail.com>
2023-10-30 20:53:57 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideDist) {
+  if (rideDist <= 400) {
+    return "This one is on me!";
+  } else if (rideDist <= 2000 && rideDist >= 400) {
+    return "That will be twenty bucks.";
+  } else if (rideDist >= 2000 && rideDist <= 2500) {
+    return "I will gladly take your thirty bucks."
+  } else {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+//ternary is amazing how it condenses
+function ternaryCheckCity(city) {
+  return city == "NYC" ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipSize) {
+  if (tipSize === 'generous') {
+    return "Thank you so much."
+  } else if (tipSize === 'not as generous') {
+    return "Thank you.";
+  } else {
+    return "Bye.";
+  }
+}
+
+//this is a switch conversion
+function switchOnCharmFromTip(tipSize) {
+  switch (tipSize) {
+    case 'generous':
+      return "Thank you so much.";
+    case 'not as generous':
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
